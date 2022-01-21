f8d650b4e1d9f0bc71d7bb4e258b171b5fdbc149
Joaquin Azua <joaquin.diego.azua@gmail.com>
2022-01-21 15:25:27 -0600

    Complete step 4 bonus add delete functionality

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Widget(models.Model):
     description = models.CharField(max_length=75)
@@ -8,3 +9,4 @@ class Widget(models.Model):
     def __str__(self):
         return self.description, self.quantity
 
+    
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,9 @@
 from venv import create
 from .models import Widget
+from django.db.models import Sum
 from django.forms import widgets
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, DeleteView
 from .forms import WidgetForm
 
 # Create your views here.
@@ -10,9 +11,15 @@ from .forms import WidgetForm
 def index(request):
     widgets = Widget.objects.all()
     widget_form = WidgetForm()
-    return render(request, 'index.html', {'widgets': widgets, 'widget_form': widget_form})
+    total_quantity = Widget.objects.all().aggregate(Sum('quantity')).get('quantity__sum')
+    
+    return render(request, 'index.html', {'widgets': widgets, 'widget_form': widget_form,'total_quantity':total_quantity })
 
 class WidgetCreate(CreateView):
     model = Widget
     fields = "__all__"
     success_url = "/"
+
+class WidgetDelete(DeleteView):
+  model = Widget
+  success_url = '/'
