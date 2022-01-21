48f242fc098185d210423368736aa2679eaa9c67
Joaquin Azua <joaquin.diego.azua@gmail.com>
2022-01-21 14:58:10 -0600

    Complete step 3.4

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,18 @@
+from venv import create
 from .models import Widget
 from django.forms import widgets
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from .forms import WidgetForm
 
 # Create your views here.
 
 def index(request):
     widgets = Widget.objects.all()
-    return render(request, 'index.html', {'widgets': widgets})
+    widget_form = WidgetForm()
+    return render(request, 'index.html', {'widgets': widgets, 'widget_form': widget_form})
+
+class WidgetCreate(CreateView):
+    model = Widget
+    fields = "__all__"
+    success_url = "/"
